e520fe3e7d3b45f889e75ff097a0beceecbe893c
mroishii <mroishii@gmail.com>
2018-08-31 14:18:33 +0900

    updated logic

--- MessageRead.js
+++ MessageRead.js
@@ -34,7 +34,8 @@
                 
         //Parameters for authenticator
         var client_id = '40f52d05-f5d8-4b29-9356-4248678802ba';
-        var configs = {redirectUrl: 'https://mroishii.github.io/MessageRead.html'};
+        var configs = {redirectUrl: 'https://mroishii.github.io/MessageRead.html',
+                       scope: 'https://graph.microsoft.com/mail.readwrite+https://graph.microsoft.com/user.read'};
 
         // register Microsoft (Azure AD 2.0 Converged auth) endpoint using parameters)
         authenticator.endpoints.registerMicrosoftAuth(client_id, configs);
